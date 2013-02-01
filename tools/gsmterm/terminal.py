c3aca55ef79b910910f939e965abf697547319dc
Francois Aucamp <francois.aucamp@gmail.com>
2013-02-01 12:37:00 +0200

    Added general/command-sensitive help

--- tools/gsmterm/terminal.py
+++ tools/gsmterm/terminal.py
@@ -13,6 +13,7 @@ import os, sys, threading
 import serial
 
 from gsmmodem.serial_comms import SerialComms
+from .trie import Trie
 
 # first choose a platform dependant way to read single characters from the console 
 global console
@@ -27,6 +28,8 @@ if os.name == 'nt':
         CURSOR_RIGHT = '{0}{1}'.format(chr(0xe0), chr(0x4d))
         #TODO: find out what this in windows:
         DELETE = ''
+        HOME = ''
+        END = ''
         
         def __init__(self):
             pass
@@ -61,6 +64,8 @@ elif os.name == 'posix':
         CURSOR_LEFT = '{0}{1}{2}'.format(chr(27), chr(91), chr(68))
         CURSOR_RIGHT = '{0}{1}{2}'.format(chr(27), chr(91), chr(67))
         DELETE = '{0}{1}{2}{3}'.format(chr(27), chr(91), chr(51), chr(126))
+        HOME = '{0}{1}{2}'.format(chr(27), chr(79), chr(72))
+        END = '{0}{1}{2}'.format(chr(27), chr(79), chr(70))
         
         def __init__(self):
             self.fd = sys.stdin.fileno()
@@ -179,6 +184,7 @@ class GsmTerm(RawTerm):
     COLOR_GREEN = COLOR_SEQ.format(30+2)
     COLOR_YELLOW = COLOR_SEQ.format(30+3)
     COLOR_BLUE = COLOR_SEQ.format(30+4)
+    COLOR_MAGENTA = COLOR_SEQ.format(30+5)
     COLOR_WHITE = COLOR_SEQ.format(30+7)
     COLOR_CYAN = COLOR_SEQ.format(30+6)
     
@@ -188,9 +194,10 @@ class GsmTerm(RawTerm):
         self.history = []
         self.historyPos = 0
         self.useColor = useColor
-        self.cursorPos = 0
+        self.cursorPos = 0        
         if self.useColor:
             self.PROMPT = self._color(self.COLOR_GREEN, self.PROMPT)
+        self._initAtCommandsTrie()
     
     def printStartMessage(self):
 #        self.stdscr.addstr('GSMTerm started. Press CTRL+] to exit.')
@@ -220,7 +227,7 @@ class GsmTerm(RawTerm):
     def _handleResponse(self, lines):
         # Clear any input prompt
         self._removeInputPrompt()
-        if line[-1] == 'ERROR':
+        if lines[-1] == 'ERROR':
             lines(self._color(self.COLOR_RED, lines))
         else:
             print(self._color(self.COLOR_CYAN, lines))
@@ -268,20 +275,10 @@ class GsmTerm(RawTerm):
                         self.cursorPos = len(self.inputBuffer)
                         self._refreshInputPrompt(clearLen)
                 elif c == '\n':
-                    # Convert newline input into \r\n
-                    if len(self.inputBuffer) > 0:
-                        inputStr = ''.join(self.inputBuffer).strip()
-                        self.inputBuffer = []
-                        self.cursorPos = 0
-                        if len(inputStr) > 0:
-                            self.serial.write(inputStr)
-                            self.serial.write(self.EOL_SEQ)                    
-                            self._addToHistory(inputStr)
-                            self.historyPos = len(self.history)                    
-                    # Locally just echo the real newline
-                    sys.stdout.write(c)
-                    sys.stdout.flush()                    
-                    
+                    self._doConfirmInput()
+                elif c == '\t':
+                    # Command completion
+                    self._doCommandCompletion()
                 elif c == self.BACKSPACE_CHARACTER:
                     if self.cursorPos > 0:
                         #print( 'cp:',self.cursorPos,'was:', self.inputBuffer)
@@ -295,16 +292,147 @@ class GsmTerm(RawTerm):
                         self.inputBuffer = self.inputBuffer[0:self.cursorPos] + self.inputBuffer[self.cursorPos+1:]                        
                         #print ('cp:', self.cursorPos,'is:', self.inputBuffer)
                         self._refreshInputPrompt(len(self.inputBuffer)+1)
+                elif c == console.HOME:
+                    self.cursorPos = 0
+                    self._refreshInputPrompt(len(self.inputBuffer))
+                elif c == console.END:
+                    self.cursorPos = len(self.inputBuffer)
+                    self._refreshInputPrompt(len(self.inputBuffer))
                 elif len(c) == 1 and self._isPrintable(c):
                     self.inputBuffer.insert(self.cursorPos, c)
                     self.cursorPos += 1
                     self._refreshInputPrompt()
-                else:
-                    for a in c:
-                        print('GOT:',a,'(',ord(a),')')
+                #else:
+                #    for a in c:
+                #        print('GOT:',a,'(',ord(a),')')
         except:
             self.alive = False
             raise
+        
+    def _doConfirmInput(self):
+        # Convert newline input into \r\n        
+        if len(self.inputBuffer) > 0:
+            inputStr = ''.join(self.inputBuffer).strip()
+            self.inputBuffer = []
+            self.cursorPos = 0
+            inputStrLen = len(inputStr)
+            if len(inputStr) > 0:
+                self._addToHistory(inputStr)
+                self.historyPos = len(self.history)
+            if inputStrLen > 2:
+                if inputStr[0] == '?': # ?COMMAND
+                    # Help requested with function                    
+                    self._printCommandHelp(inputStr[1:])
+                    return
+                elif inputStr[-1] == inputStr[-2] == '?': # COMMAND??
+                    # Help requested with function
+                    cmd = inputStr[:-3 if inputStr[-3] == '=' else -2]                
+                    self._printCommandHelp(cmd)
+                    return
+            if inputStr.lower().startswith('help'): # help COMMAND
+                # Alternative help invocation
+                self._printCommandHelp(inputStr[5:])
+                return            
+            if len(inputStr) > 0:
+                self.serial.write(inputStr)
+                self.serial.write(self.EOL_SEQ)
+        # Locally just echo the real newline
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+    
+    
+    def _printGeneralHelp(self):
+        sys.stdout.write(self._color(self.COLOR_WHITE, '\n\n== GSMTerm Help ==\n\n'))        
+        sys.stdout.write('{0} Press the up & down arrow keys to move backwards or forwards through your command history.\n\n'.format(self._color(self.COLOR_YELLOW, 'Command History:')))
+        sys.stdout.write('{0} Press the TAB key to provide command completion suggestions. Press the TAB key after a command is fully typed (with or without a "=" character) to quickly see its syntax.\n\n'.format(self._color(self.COLOR_YELLOW, 'Command Completion:')))
+        sys.stdout.write('{0} Type a command, followed with two quesetion marks to access its documentation, e.g. "<COMMAND>??". Alternatively, precede the command with a question mark ("?<COMMAND>"), or type "help <COMMAND>".\n\n'.format(self._color(self.COLOR_YELLOW, 'Command Documentation:')))
+        sys.stdout.write('To exit GSMTerm, press CTRL+] or CTRL+D.\n\n')
+        self._refreshInputPrompt(len(self.inputBuffer))
+    
+    def _printCommandHelp(self, command=None):
+        if command == None or len(command.strip()) == 0:
+            # Print general help
+            self._printGeneralHelp()
+            return
+        try:
+            command = command.strip()
+            commandHelp = self.completion[command]
+        except KeyError:
+            noHelp = True
+        else:
+            noHelp = commandHelp == None
+        if noHelp:
+            sys.stdout.write('\r No help available for: {0}\n'.format(self._color(self.COLOR_WHITE, command)))
+        else:
+            sys.stdout.write('\n\n{0} ({1})\n\n'.format(self._color(self.COLOR_WHITE, commandHelp[1]), command))
+            sys.stdout.write('{0} {1}\n'.format(self._color(self.COLOR_YELLOW, 'Description:'), commandHelp[4]))
+            sys.stdout.write('{0} '.format(self._color(self.COLOR_YELLOW, 'Values:')))
+            commandValues = commandHelp[2]
+            syntax = [self._color(self.COLOR_WHITE, command)]
+            if commandValues != None:
+                syntax.append(self._color(self.COLOR_WHITE, '='))
+                sys.stdout.write('\n')
+                for value, valueDesc in commandValues:
+                    syntax.append(self._color(self.COLOR_MAGENTA, value))
+                    syntax.append(' ')
+                    sys.stdout.write(' {0} {1}\n'.format(self._color(self.COLOR_MAGENTA, value), valueDesc.replace('\n', '\n' + ' ' * (len(value) + 2)) if valueDesc != None else ''))
+            else:
+                sys.stdout.write('No parameters.\n')
+            sys.stdout.write('{0}\n {1}\n\n'.format(self._color(self.COLOR_YELLOW, 'Command Syntax:'), ''.join(syntax)))
+        self._refreshInputPrompt(len(self.inputBuffer))
+    
+    def _doCommandCompletion(self):
+        """ Command-completion method """        
+        prefix =  ''.join(self.inputBuffer).strip()
+        matches = self.completion.keys(prefix)
+        matchLen = len(matches)
+        if matches == 0 and prefix[-1] == '=':
+            try:
+                command = self.completion[prefix[:-1]]
+            except KeyError:
+                pass                        
+            else:
+                self.__printCommandSyntax(command)
+        elif matchLen > 0:                        
+            if matchLen == 1:
+                if matches[0] == prefix:
+                    # User has already entered command - show command syntax
+                    self.__printCommandSyntax(prefix)
+                else:
+                    # Complete only possible command
+                    self.inputBuffer = list(matches[0])
+                    self.cursorPos = len(self.inputBuffer)
+                    self._refreshInputPrompt(len(self.inputBuffer))
+                return
+            else:
+                commonPrefix = self.completion.longestCommonPrefix(''.join(self.inputBuffer))
+                self.inputBuffer = list(commonPrefix)
+                self.cursorPos = len(self.inputBuffer)
+                if matchLen > 20:
+                    matches = matches[:20]
+                    matches.append('...')
+            sys.stdout.write('\n')
+            for match in matches:
+                sys.stdout.write(' {0} '.format(match))
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+            self._refreshInputPrompt(len(self.inputBuffer))
+    
+    def __printCommandSyntax(self, command):
+        """ Command-completion helper method: print command syntax """
+        commandHelp = self.completion[command]
+        if commandHelp != None:
+            commandValues = commandHelp[2]
+            #commandDefault = commandHelp[3]
+            displayHelp = [self._color(self.COLOR_WHITE, command)]
+            if commandValues != None:
+                displayHelp.append(self._color(self.COLOR_WHITE, '='))
+                for value in commandValues:
+                    displayHelp.append(self._color(self.COLOR_MAGENTA, value[0]))
+                    displayHelp.append(' ')
+            sys.stdout.write('\r Syntax: {0}\n'.format(self._color(self.COLOR_WHITE, ''.join(displayHelp))))
+            sys.stdout.flush()
+            self._refreshInputPrompt(len(self.inputBuffer))
     
     def _isPrintable(self, char):
         return 33 <= ord(char) <= 126 or char.isspace()  
@@ -317,60 +445,12 @@ class GsmTerm(RawTerm):
     def _removeInputPrompt(self):
         sys.stdout.write('\r{0}\r'.format(' ' * (len(self.PROMPT) + len(self.inputBuffer))))
     
-#    def start(self):    
-#        self.stdscr = curses.initscr()
-#        curses.noecho()
-#        curses.cbreak()
-#        self.stdscr.keypad(1)
-#        curses.start_color()
-#        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)        
-#        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-#        curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
-#        super(GsmTerm, self).start()        
-    
-#    def stop(self):
-#        curses.nocbreak();
-#        self.stdscr.keypad(0)
-#        curses.echo()
-#        curses.endwin()
-#        super(GsmTerm, self).stop()
-        
-    
- 
-
-    
-    
-#    def _inputLoop(self):
-#        """ Loop and copy console->serial until EXIT_CHARCTER character is found. """
-#        try:
-#            while self.alive:
-#                d = self.stdscr.getch()
-                
-                #if d == 
-                
-#                c = chr(d)
-#                self.stdscr.addstr('{0}:{1}\n'.format(d,c), curses.color_pair(3))
-#                if c == self.EXIT_CHARACTER:
-#                    self.stop()
-#                elif c == '\n':
-                    # Convert newline input into \r\n
-#                    self.serial.write(self.EOL_SEQ)
-#                    if self.echo:
-                        # Locally just echo the real newline
-#                        self.stdscr.addstr(c, )
-#                        sys.stdout.flush()
-#                else:
-                    #print('writing: ', c)
-                    #self.
-#                    self.serial.write(c)
-#                    if self.echo:
-#                        self.stdscr.addstr(c)
-#                        sys.stdout.flush()
-#        except:
-#            self.alive = False
-#            raise
-
-
-
-
+    def _initAtCommandsTrie(self):
+        self.completion = Trie()
+        from .atcommands import ATCOMMANDS
+        for command, help in ATCOMMANDS:
+            if help != None:
+                self.completion[command] = help
+            else:
+                self.completion[command] = None
 
