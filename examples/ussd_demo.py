33b7ba4e9aabf361db9f16c89d5406d10cdb4503
Francois Aucamp <francois.aucamp@gmail.com>
2013-02-27 14:09:54 +0200

    Added USSD example script

---
+++ examples/ussd_demo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""\
+Demo: Simple USSD example
+
+Simple demo app that initiates a USSD session, reads the string response and closes the session
+(if it wasn't closed by the network)
+
+Note: for this to work, a valid USSD string for your network must be used.
+"""
+
+from __future__ import print_function
+
+PORT = '/dev/ttyUSB2'
+BAUDRATE = 9600
+USSD_STRING = '*101#'
+
+from gsmmodem.modem import GsmModem
+
+def main():
+    modem = GsmModem(PORT, BAUDRATE)
+    modem.connect()
+    modem.waitForNetworkCoverage(10)
+    print('Sending USSD string: {}'.format(USSD_STRING))
+    response = modem.sendUssd(USSD_STRING) # response type: gsmmodem.modem.Ussd
+    print('USSD reply received: {}'.format(response.message))
+    if response.sessionActive:
+        print('Closing USSD session.')
+        # At this point, you could also reply to the USSD message by using response.reply()
+        response.cancel()
+    else:
+        print('USSD session was ended by network.')
+    modem.close()
+
+if __name__ == '__main__':
+    main()
