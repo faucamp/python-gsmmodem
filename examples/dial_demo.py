dfb8774b8ba00409ccd30a9c539673bcef525ea8
Francois Aucamp <francois.aucamp@gmail.com>
2013-05-10 16:37:02 +0200

    Cleanup demo script

--- examples/dial_demo.py
+++ examples/dial_demo.py
@@ -21,30 +21,6 @@ PIN = None # SIM card PIN (if any)
 from gsmmodem.modem import GsmModem
 from gsmmodem.exceptions import InterruptedException, CommandError
 
-def handleIncomingCall(call):
-    if call.ringCount == 1:
-        print('Incoming call from:', call.number)
-    elif call.ringCount >= 2:
-        if call.dtmfSupport:
-            print('Answering call and playing some DTMF tones...')
-            call.answer()
-            # Wait for a bit - some older modems struggle to send DTMF tone immediately after answering a call
-            time.sleep(2.0) 
-            try:
-                call.sendDtmfTone('9515999955951')
-            except InterruptedException as e:
-                # Call was ended during playback
-                print('DTMF playback interrupted: {0} ({1} Error {2})'.format(e, e.cause.type, e.cause.code))                
-            finally:
-                if call.answered:
-                    print('Hanging up call.')
-                    call.hangup()
-        else:            
-            print('Modem has no DTMF support - hanging up call.')
-            call.hangup()
-    else:
-        print(' Call from {0} is still ringing...'.format(call.number))
-    
 def main():
     if NUMBER == None or NUMBER == '00000':
         print('Error: Please change the NUMBER variable\'s value before running this example.')
