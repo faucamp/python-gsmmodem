b1825d8bf1ccf0bab15006b26ff5c9a3c8c20fc5
Benny Simonsen <benny@slbs.dk>
2016-08-20 08:19:40 +0200

    Re-added _placeHolderCallback
    It is needed as a default callback function if no callback is specified.
    
    Fixes craching while sending SMS without specifing
    smsStatusReportCallback.
    
    I have also fixed for smsReceivedCallbackFunc, but didn't get a crash
    because I had a call back function.

--- gsmmodem/modem.py
+++ gsmmodem/modem.py
@@ -153,8 +153,8 @@ class GsmModem(SerialComms):
     def __init__(self, port, baudrate=115200, incomingCallCallbackFunc=None, smsReceivedCallbackFunc=None, smsStatusReportCallback=None, requestDelivery=True, AT_CNMI="", *a, **kw):
         super(GsmModem, self).__init__(port, baudrate, notifyCallbackFunc=self._handleModemNotification, *a, **kw)
         self.incomingCallCallback = incomingCallCallbackFunc or self._placeholderCallback
-        self.smsReceivedCallback = smsReceivedCallbackFunc
-        self.smsStatusReportCallback = smsStatusReportCallback
+        self.smsReceivedCallback = smsReceivedCallbackFunc or self._placeholderCallback
+        self.smsStatusReportCallback = smsStatusReportCallback or self._placeholderCallback
         self.requestDelivery = requestDelivery
         self.AT_CNMI = AT_CNMI or "2,1,0,2"
         # Flag indicating whether caller ID for incoming call notification has been set up
@@ -1443,6 +1443,10 @@ class GsmModem(SerialComms):
             sessionActive = cusdMatches[0].group(1) == '1'
             message = cusdMatches[0].group(2)
         return Ussd(self, sessionActive, message)
+
+    def _placeHolderCallback(self, *args):
+        """ Does nothing """
+        self.log.debug('called with args: {0}'.format(args))
 
     def _pollCallStatus(self, expectedState, callId=None, timeout=None):
         """ Poll the status of outgoing calls.
