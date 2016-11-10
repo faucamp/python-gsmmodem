19c0a5cbfce9e4bb86e927df0457425e48e32290
babca <github@email.cz>
2016-11-10 16:39:50 +0100

    v0.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
     test_command = [sys.executable, '-m', 'unittest', 'discover']
     coverage_command = ['coverage', 'run', '-m', 'unittest', 'discover']
 
-VERSION = "0.10"
+VERSION = "0.11"
 
 class RunUnitTests(Command):
     """ run unit tests """
