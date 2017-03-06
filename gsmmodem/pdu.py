e500f311c21264ac60caf3f5f7f7ef55e111d2ad
babca <github@email.cz>
2017-03-06 22:01:25 +0100

    Better alphanumeric address decoding fix

--- gsmmodem/pdu.py
+++ gsmmodem/pdu.py
@@ -547,11 +547,8 @@ def _decodeDataCoding(octet):
     # We ignore other coding groups
     return 0
 
-def nibble2octet(o):
-    if o % 2:
-        return o / 2 + 1
-    else:
-        return o / 2
+def nibble2octet(addressLen):
+    addressLen = int((addressLen + 1) / 2)
 
 def _decodeAddressField(byteIter, smscField=False, log=False):
     """ Decodes the address field at the current position of the bytearray iterator
