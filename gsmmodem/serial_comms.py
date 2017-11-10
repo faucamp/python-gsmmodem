bd7c54f3116643ffd68995481f033a424551b88c
babca <babic@plutanium.cz>
2017-11-10 13:22:50 +0100

    _placeholderCallback removed from serial_comms.py too
    This is not a fix and does not add any functionality. I made changes to
    a way of calling callback so it reflects recent changes in modem.py.

--- gsmmodem/serial_comms.py
+++ gsmmodem/serial_comms.py
@@ -39,8 +39,8 @@ class SerialComms(object):
         # Reentrant lock for managing concurrent write access to the underlying serial port
         self._txLock = threading.RLock()
 
-        self.notifyCallback = notifyCallbackFunc or self._placeholderCallback
-        self.fatalErrorCallback = fatalErrorCallbackFunc or self._placeholderCallback
+        self.notifyCallbackFunc = notifyCallbackFunc
+        self.fatalErrorCallbackFunc = fatalErrorCallbackFunc
 
         self.com_args = args
         self.com_kwargs = kwargs
@@ -78,11 +78,9 @@ class SerialComms(object):
                 # No more chars on the way for this notification - notify higher-level callback
                 #print 'notification:', self._notification
                 self.log.debug('notification: %s', self._notification)
-                self.notifyCallback(self._notification)
+                if self.notifyCallbackFunc:
+                    self.notifyCallbackFunc(self._notification)
                 self._notification = []
-
-    def _placeholderCallback(self, *args, **kwargs):
-        """ Placeholder callback function (does nothing) """
 
     def _readLoop(self):
         """ Read thread main loop
@@ -119,7 +117,8 @@ class SerialComms(object):
             except Exception: #pragma: no cover
                 pass
             # Notify the fatal error handler
-            self.fatalErrorCallback(e)
+            if self.fatalErrorCallbackFunc:
+                self.fatalErrorCallbackFunc(e)
 
     def write(self, data, waitForResponse=True, timeout=5, expectedResponseTermSeq=None):
         data = data.encode()
